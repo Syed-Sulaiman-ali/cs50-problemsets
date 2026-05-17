9a1e1fb71b228698712f89139138c800335f67dc
Sulaiman Ali <syedsulaiman1240@gmail.com>
2026-05-17 07:40:44 +0530

    Updated mario.c to correct the spelling of "Height".

---
+++ Problem Set 1/mario-less/mario.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+    // printing right-aligned staircase.
+
+    // get input from the user by using the library 'cs50.h'
+
+    int n;
+    do
+    {
+        n = get_int("Height: ");
+    } while (n < 1);
+
+    // this outer loops is for printing rows
+    for (int row = 1; row <= n; row++)
+    {
+        // This loops print spaces before '#'
+        for (int space = 0; space < n - row; space++)
+        {
+            printf(" ");
+        }
+
+        // this loops print '#'
+        for (int column = 0; column < row; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
